29fc8f0a048988ecb652667fd2182e214fa5778d
hoiheart <hoiheart@gmail.com>
2019-06-06 10:54:37 +0900

    :hammer: Deploy Settings

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,3 +1,3 @@
 export function getStaticPath(): string {
-  return process.env.NODE_ENV === 'production' ? 'https://epl18-9164b.web.app/static' : 'http://localhost:5000/static';
+  return process.env.NODE_ENV === 'production' ? 'https://epl18-9164b.web.app/_next/static' : 'http://localhost:5000/static';
 }
