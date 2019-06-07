2400fcfb192240bf2d1ec2f96f5365ac8d69091e
hoiheart <hoiheart@gmail.com>
2019-06-07 16:36:15 +0900

    🏢 Markup

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -11,70 +11,76 @@ interface IIndex {
   standings?: [];
 }
 
-const Index: NextFunctionComponent<IIndex> = ({ standings }) => {
+const Index: NextFunctionComponent<IIndex> = ({ standings = [] }) => {
   // const count = useSelector((state) => state.count, shallowEqual);
+  const renderStandings = () => (
+    <>
+      <h2>Standings</h2>
+      <div className="standings">
+        <table>
+          <thead>
+            <tr>
+              <th scope="col" className="rank">Rank</th>
+              <th scope="col" className="name">Team</th>
+              <th scope="col" className="gp">GP</th>
+              <th scope="col" className="win">W</th>
+              <th scope="col" className="draw">D</th>
+              <th scope="col" className="lose">L</th>
+              <th scope="col" className="point">P</th>
+              <th scope="col" className="gf">GF</th>
+              <th scope="col" className="ga">GA</th>
+              <th scope="col" className="gd">GD</th>
+            </tr>
+          </thead>
+          <tbody>
+            {renderStandingsList()}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+
+  const renderStandingsList = () => (
+    standings.map((v: any, index) => (
+    <tr key={index} style={{backgroundColor: v.note ? v.note.color : false}}>
+      <td className="rank">{v.stats[7].displayValue}</td>
+      <td className="name">
+        <Link href={`/teams?id=${v.team.id}`}>
+          <a>
+            <img src={v.team.logos[0].href} width="48" height="48" alt="" />
+            {v.team.displayName}
+          </a>
+        </Link>
+      </td>
+      <td className="gp">{v.stats[3].displayValue}</td>
+      <td className="win">{v.stats[0].displayValue}</td>
+      <td className="draw">{v.stats[2].displayValue}</td>
+      <td className="lose">{v.stats[1].displayValue}</td>
+      <td className="point">{v.stats[6].displayValue}</td>
+      <td className="gf">{v.stats[4].displayValue}</td>
+      <td className="ga">{v.stats[5].displayValue}</td>
+      <td className="gd">{v.stats[8].displayValue}</td>
+    </tr>
+    ))
+  );
+
   return (
     <Layout title="Home | English Premier League 2018">
-      {standings.length &&
-      <>
-        <h2>Standings</h2>
-        <div className="standings">
-          <table>
-            <thead>
-              <tr>
-                <th scope="col" className="rank">Rank</th>
-                <th scope="col" className="name">Team</th>
-                <th scope="col" className="gp">GP</th>
-                <th scope="col" className="win">W</th>
-                <th scope="col" className="draw">D</th>
-                <th scope="col" className="lose">L</th>
-                <th scope="col" className="point">P</th>
-                <th scope="col" className="gf">GF</th>
-                <th scope="col" className="ga">GA</th>
-                <th scope="col" className="gd">GD</th>
-              </tr>
-            </thead>
-            <tbody>
-              {standings.map((v, index) => (
-              <tr key={index} style={{'backgroundColor': v.note ? v.note.color : false}}>
-                <td className="rank">{v.stats[7].displayValue}</td>
-                <td className="name">
-                  <Link href={`/teams?id=${v.team.id}`}>
-                    <a>
-                      <img src={v.team.logos[0].href} width="48" height="48" alt="" />
-                      {v.team.displayName}
-                    </a>
-                  </Link>
-                </td>
-                <td className="gp">{v.stats[3].displayValue}</td>
-                <td className="win">{v.stats[0].displayValue}</td>
-                <td className="draw">{v.stats[2].displayValue}</td>
-                <td className="lose">{v.stats[1].displayValue}</td>
-                <td className="point">{v.stats[6].displayValue}</td>
-                <td className="gf">{v.stats[4].displayValue}</td>
-                <td className="ga">{v.stats[5].displayValue}</td>
-                <td className="gd">{v.stats[8].displayValue}</td>
-              </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </>
-      }
+      {standings.length ? renderStandings() : ''}
     </Layout>
   );
 };
 
-Index.getInitialProps = async (props) => {
+Index.getInitialProps = async () => {
   let result: IIndex = {
-    standings: []
+    standings: [],
   };
 
   try {
     const res = await fetch(`${getStaticPath()}/data/standings/standings.json`);
     const json = await res.json();
     result = { ...res, standings: json.children[0].standings.entries };
-  } catch(e) {}
+  } catch (e) {}
 
   return result;
 };
--- src/app/pages/teams.tsx
+++ src/app/pages/teams.tsx
@@ -9,112 +9,123 @@ import Layout from '../components/Layout';
 
 interface ITeams {
   teams?: [];
-  detail?: {};
+  team?: any;
   roster?: [];
 }
 
-const Teams: NextFunctionComponent<ITeams> = ({ teams, team, roster }) => {
+const Teams: NextFunctionComponent<ITeams> = ({ teams = [], team = {}, roster = [] }) => {
   // const count = useSelector((state) => state.count, shallowEqual);
-  console.log(team, roster)
+  const renderTeams = () => (
+    <>
+      <h2>Teams</h2>
+      <div className="teams">
+        <ul>
+          {renderTeamsList()}
+        </ul>
+      </div>
+    </>
+  );
+  const renderTeamsList = () => (
+    teams.map((v: any, index) => (
+      <li key={index}>
+        <Link scroll={false} href={`/teams?id=${v.team.id}`}>
+          <a>
+            <img src={v.team.logos[0].href} width="48" height="48" alt="" />
+            {v.team.displayName}
+          </a>
+        </Link>
+      </li>
+    ))
+  );
+  const renderTeam = () => (
+    <>
+      <div className="team">
+        <div className="logo"><img src={team.logos[0].href} width="64" height="64" alt="" /></div>
+        <h3>{team.displayName}<small className="nickname"> ({team.nickname})</small></h3>
+        <div className="record">Record: {team.record.items[0].summary}</div>
+        <div className="rank">Rank: {team.record.items[0].stats[8].value}</div>
+      </div>
+    </>
+  );
+  const renderRoster = () => (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col" className="number">No.</th>
+            <th scope="col" className="position">Position</th>
+            <th scope="col" className="name">Name</th>
+            <th scope="col" className="country">Country</th>
+            <th scope="col" className="age">Age</th>
+            <th scope="col" className="goals">Goals</th>
+            <th scope="col" className="assists">Assists</th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderRosterList()}
+        </tbody>
+      </table>
+    </>
+  );
+  const renderRosterList = () => (
+    roster.map((v: any, index) => {
+      return (
+        <tr key={index}>
+          <td className="number">{v.jersey}</td>
+          <td className="position">{v.position.abbreviation}</td>
+          <td className="name">
+            <Link href={`/player?id=${v.id}`}>
+              <a>{v.fullName}</a>
+            </Link>
+          </td>
+          <td scope="col" className="country">
+            {v.flag ? <><img src={v.flag.href} width="32" height="32" alt="" /> {v.flag.alt}</> : '-'}
+          </td>
+          <td scope="col" className="age">{v.age}</td>
+          <td scope="col" className="goals">
+            {v.statistics ? v.statistics.splits.categories[1].stats[4].displayValue : '0'}
+          </td>
+          <td scope="col" className="assists">
+            {v.statistics ? v.statistics.splits.categories[1].stats[0].displayValue : '0'}
+          </td>
+        </tr>
+      );
+    })
+  );
   return (
     <Layout title="Home | EPL18">
-      {teams.length &&
-      <>
-        <h2>Teams</h2>
-        <div className="teams">
-          <ul>
-            {teams.map((v, index) => (
-              <li key={index}>
-                <Link scroll={false} href={`/teams?id=${v.team.id}`}>
-                  <a>
-                    <img src={v.team.logos[0].href} width="48" height="48" alt="" />
-                    {v.team.displayName}
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </>
-      }
-      {team.id &&
-      <>
-        <div className="team">
-          <div className="logo"><img src={team.logos[0].href} width="64" height="64" alt="" /></div>
-          <h3>{team.displayName}<small className="nickname"> ({team.nickname})</small></h3>
-          <div className="record">Record: {team.record.items[0].summary}</div>
-          <div className="rank">Rank: {team.record.items[0].stats[8].value}</div>
-        </div>
-      </>
-      }
-      {roster.length &&
-      <>
-        <table>
-          <thead>
-            <tr>
-              <th scope="col" className="number">No.</th>
-              <th scope="col" className="position">Position</th>
-              <th scope="col" className="name">Name</th>
-              <th scope="col" className="country">Country</th>
-              <th scope="col" className="age">Age</th>
-              <th scope="col" className="goals">Goals</th>
-              <th scope="col" className="assists">Assists</th>
-            </tr>
-          </thead>
-          <tbody>
-            {roster.map((v, index) => {
-              console.log(v.statistics)
-              return (
-                <tr key={index}>
-                  <td className="number">{v.jersey}</td>
-                  <td className="position">{v.position.abbreviation}</td>
-                  <td className="name">
-                    <Link href={`/player?id=${v.id}`}>
-                      <a>{v.fullName}</a>
-                    </Link>
-                  </td>
-                  <td scope="col" className="country">
-                    {v.flag ? <><img src={v.flag.href} width="32" height="32" alt="" /> {v.flag.alt}</> : '-'}
-                  </td>
-                  <td scope="col" className="age">{v.age}</td>
-                  <td scope="col" className="goals">{v.statistics ? v.statistics.splits.categories[1].stats[4].displayValue : '0'}</td>
-                  <td scope="col" className="assists">{v.statistics ? v.statistics.splits.categories[1].stats[0].displayValue : '0'}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </>
-      }
+      {teams.length ? renderTeams() : ''}
+      {team.id ? renderTeam() : ''}
+      {roster.length ? renderRoster() : ''}
     </Layout>
   );
 };
 
-Teams.getInitialProps = async (props) => {
+Teams.getInitialProps = async (props: any) => {
   let result: ITeams = {
     teams: [],
     team: {},
-    roster: []
+    roster: [],
   };
 
   try {
     const res = await fetch(`${getStaticPath()}/data/teams/teams.json`);
     const json = await res.json();
     result = { ...result, teams: json.sports[0].leagues[0].teams };
-  } catch(e) {}
+  } catch (e) {}
 
   if (props.ctx.query.id) {
     try {
       const res = await fetch(`${getStaticPath()}/data/teams/${props.ctx.query.id}.json`);
       const json = await res.json();
       result = { ...result, team: json.team };
-    } catch(e) {}
+    } catch (e) {}
 
     try {
       const res = await fetch(`${getStaticPath()}/data/rosters/${props.ctx.query.id}.json`);
       const json = await res.json();
       result = { ...result, roster: json.athletes };
-    } catch(e) {}
+    } catch (e) {}
   }
 
   return result;
