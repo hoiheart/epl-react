2780b4578d136833dd0ebfe88912a308deb531a9
hoiheart <hoiheart@gmail.com>
2020-06-23 21:40:57 +0900

    date format

--- components/fixtures/nav.tsx
+++ components/fixtures/nav.tsx
@@ -9,12 +9,13 @@ interface FixturesNavComponent {
 
 const FixturesNav: React.FunctionComponent<FixturesNavComponent> = ({ date }) => {
   const yesterday = dayjs(date).add(-1, 'day').format('YYYYMMDD')
+  const today = dayjs(date).format('YYYY-MM-DD (ddd)')
   const tomorrow = dayjs(date).add(1, 'day').format('YYYYMMDD')
 
   return (
     <div className='fixtures-nav'>
       <Link href={`/fixtures?date=${yesterday}`}><a>&lt;</a></Link>
-      {date}
+      {today}
       <Link href={`/fixtures?date=${tomorrow}`}><a>&gt;</a></Link>
     </div>
   )
